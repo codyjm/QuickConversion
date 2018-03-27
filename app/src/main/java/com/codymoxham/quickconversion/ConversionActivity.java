c5cae5c42755cc7c07a9a0e9fbf56b551c6a6140
Cody Moxham <jc281092@ad.jcu.edu.au>
2018-03-27 23:47:08 +1000

    Initial commit

---
+++ app/src/main/java/com/codymoxham/quickconversion/ConversionActivity.java
@@ -0,0 +1,13 @@
+package com.codymoxham.quickconversion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConversionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversion);
+    }
+}
